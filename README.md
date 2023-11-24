b99dbc69bb7e6a3fa4ececee362f4c85119b1f0b
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2023-11-24 09:06:23 +0200

    add base of project

---
+++ README.md
@@ -0,0 +1,1 @@
+# The project on React using hooks 
---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,34 @@
+import React, {useState} from "react";
+
+function GuessInput(){
+    const [guess, setGuess] = useState('');
+    function handleSubmit(event){
+        event.preventDefault();
+        if(guess.length !==5){
+            window.alert('Please enter 5 characters')
+            return
+        }
+        console.log({guess})
+        setGuess('')
+    } 
+    return(
+    <form 
+        onSubmit={handleSubmit}
+        className="guess-input-wrapper">
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input 
+        required
+        minLength={5}
+        maxLength={5}
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(event) => {
+            const nextGuess= event.target.value.toUpperCase()
+            setGuess(nextGuess);
+        }}
+         />
+    </form>
+    )
+}
+export default GuessInput;
