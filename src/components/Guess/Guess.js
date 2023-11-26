0ec037d4f559ed378de8bf9847c4729a43878046
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2023-11-26 19:23:21 +0200

    show interface of project

---
+++ src/components/Guess/Guess.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+
+function Guess({value}){
+return(
+    <div class="guess-results">
+  <p class="guess">
+    {}
+{/*     <span className="cell">H</span>
+    <span className="cell">E</span>
+    <span className="cell">L</span>
+    <span className="cell">L</span>
+    <span className="cell">O</span> */}
+  </p>
+  
+</div>
+)
+}
+
+export default Guess;
