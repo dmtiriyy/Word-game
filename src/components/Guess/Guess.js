e4c97a619fee0ba2ccd2716ffc908b1f97d7a824
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2023-11-27 22:03:29 +0200

    add interface of words

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,17 +1,14 @@
 import React from "react";
 
-
+import { range} from '../../utils';
 
 function Guess({value}){
 return(
     <div class="guess-results">
   <p class="guess">
-    {}
-{/*     <span className="cell">H</span>
-    <span className="cell">E</span>
-    <span className="cell">L</span>
-    <span className="cell">L</span>
-    <span className="cell">O</span> */}
+    {range(5).map((num) => (
+      <span key={num} className="cell">{value? value[num]: undefined}</span> 
+    ))}
   </p>
   
 </div>
