0fb41dfd9fd0a846f3cebe8ce342bf12c8382e1b
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2023-12-25 21:09:18 +0200

    add async func

--- src/components/LostBanner/LostBanner.js
+++ src/components/LostBanner/LostBanner.js
@@ -2,7 +2,18 @@ import React from "react";
 
 import Banner from "../Banner/Banner";
 
+async function handleWrong(){
+    const url = `wrongTerm/id`;
+    const response = await fetch(url);
+    const json = await response.json();
+
+    if(json.ok){
+        return true;
+    }
+}
+
 function LostBanner({answer}){
+
     return <Banner status="sad">
 <p> Sorry, the correct answer is <strong>{answer}</strong>
             </p>
