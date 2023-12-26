9ae060a528bfdb5eba130bbe35687b404517ad6f
dmtiriyy <dim.evtushenckoyandex.ua@gmail.com>
2023-12-26 23:34:37 +0200

    edit async

--- src/components/LostBanner/LostBanner.js
+++ src/components/LostBanner/LostBanner.js
@@ -2,18 +2,19 @@ import React from "react";
 
 import Banner from "../Banner/Banner";
 
-async function handleWrong(){
-    const url = `wrongTerm/id`;
-    const response = await fetch(url);
-    const json = await response.json();
 
-    if(json.ok){
-        return true;
-    }
-}
 
 function LostBanner({answer}){
-
+    const [answers,setAnswers] = React.useState('')
+    async function handleWrong(){
+        const url = `wrongTerm/id`;
+        const response = await fetch(url);
+        const json = await response.json();
+    
+        if(json.ok){
+            setAnswers(answers)
+        }
+    }
     return <Banner status="sad">
 <p> Sorry, the correct answer is <strong>{answer}</strong>
             </p>
